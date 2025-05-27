7620ef54d3121cfbacf1c1857d636c4af20ed19f
fayzullayevelmurod <elmurodfayzullayev77@gmail.com>
2025-05-27 19:31:59 +0500

    main page the end

--- assets/js/main.js
+++ assets/js/main.js
@@ -60,3 +60,18 @@ const swiper3 = new Swiper('.brandSwiper', {
         },
     },
 });
+
+const tabButtons = document.querySelectorAll('.tab-btn');
+const tabContents = document.querySelectorAll('.tab-content');
+
+tabButtons.forEach(btn => {
+    btn.addEventListener('click', () => {
+        tabButtons.forEach(b => b.classList.remove('active'));
+        tabContents.forEach(content => content.classList.remove('active'));
+
+        btn.classList.add('active');
+        const target = document.getElementById(btn.dataset.tab);
+        target.classList.add('active');
+    });
+});
+
